b94e4a1372d1d6df7896ddc22fef013dfa38830d
Matias Guerrero <matias.f.guerrero19@gmail.com>
2023-06-09 00:16:35 -0300

    imagen test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import MainLayout from '@/components/layouts/MainLayout';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,9 @@ export default function Home() {
     <MainLayout>
       <>
         <h1>Home Page</h1>
+        <br/>
+        <img alt="destiny" src="https://i.imgur.com/DgjWUMI.png" />
+        <br/>
         <h1 className='title'>
           Ir a <Link href='/about'>About</Link>
         </h1>
